d710a535060761cf811435d6dcc17fb51b3f5b94
mhshifat <mehedihassanshifat01@gmail.com>
2020-09-27 18:05:38 +0600

    Added cart, login, register pages

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const path = require("path");
 const express = require("express");
-const expressEjsLayouts = require("express-ejs-layouts");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const PORT = process.env.PORT || "5000";
 
-// Set Template Engine...
-app.set(expressEjsLayouts);
-app.set("views", path.join(__dirname, "./resources/views"));
-app.set("view engine", "ejs");
+app.use(expressLayouts);
 // Assets...
 app.use(express.static("public"));
+// Set Template Engine...
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./resources/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
 });
 
 app.listen(PORT, () => {
